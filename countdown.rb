3414c87fd7b2afd8a6048854efdaf3e23ff0e104
Ethan <ethansagin@gmail.com>
2019-03-11 22:25:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,5 +10,13 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  
+def countdown_with_sleep(number)
+    counter = number
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+    sleep
+    break if counter == 0
+  end
+  return "HAPPY NEW YEAR!"
+end
